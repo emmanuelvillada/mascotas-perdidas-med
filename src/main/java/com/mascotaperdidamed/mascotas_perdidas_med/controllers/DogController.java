87cbec8038da64904eeb7b3d6d3e8005d4a6e76b
Emmanuel Villada Suarez <emmanuelvillada1903@gmail.com>
2025-02-21 20:32:55 -0500

    creating Dog repository

--- src/main/java/com/mascotaperdidamed/mascotas_perdidas_med/controllers/DogController.java
+++ src/main/java/com/mascotaperdidamed/mascotas_perdidas_med/controllers/DogController.java
@@ -5,16 +5,38 @@ import com.mascotaperdidamed.mascotas_perdidas_med.services.DogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @RestController
 public class DogController {
-    private final DogService dogService;
+    
+    @Autowired
+    private DogService dogService;
 
-    public DogController(DogService dogService) {
-        this.dogService = dogService;
-        dogService.
+    @GetMapping("/dogs")
+    public ResponseEntity<List<Dog>> getAllDogs() {
+        List<Dog> dogs = dogService.getAllDogs();
+        return new ResponseEntity<>(dogs, HttpStatus.OK);
+    }
+    
+    @GetMapping("/dogs/{id}")
+    public ResponseEntity<Dog> getDogById(@PathVariable Long id) {
+        Dog dog = dogService.getDogById(id);
+        return new ResponseEntity<>(dog, HttpStatus.OK);
+    }
+
+    @PostMapping("/dogs")
+    public ResponseEntity<Dog> createDog(@RequestBody Dog dog) {
+        Dog createdDog = dogService.createDog(dog);
+        return new ResponseEntity<>(createdDog, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/dogs/{id}")
+    public ResponseEntity<Dog> updateDog(@PathVariable Long id, @RequestBody Dog dog) {
+        Dog updatedDog = dogService.updateDog(id, dog);
+        return new ResponseEntity<>(updatedDog, HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/com/mascotaperdidamed/mascotas_perdidas_med/repositories/DogRepository.java
@@ -0,0 +1,21 @@
+package com.mascotaperdidamed.mascotas_perdidas_med.repositories;
+
+import java.util.List;
+
+import com.mascotaperdidamed.mascotas_perdidas_med.models.Dog;
+
+public class DogRepository {
+
+    public List<Dog> getAllDogs() {
+        return null;
+    }
+
+    public Dog getDogById(Long id) {
+        return null;
+    }
+
+    public Dog createDog(Dog dog) {
+        return null;
+    }
+    
+}
